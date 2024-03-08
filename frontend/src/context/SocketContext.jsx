551133105ecfd1fca900902f92a55ab317aa8917
Burak Orkmez <burakorkmezz@gmail.com>
2024-03-08 23:07:44 +0300

    socket url update for deployment

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 	const user = useRecoilValue(userAtom);
 
 	useEffect(() => {
-		const socket = io("http://localhost:5000", {
+		const socket = io("/", {
 			query: {
 				userId: user?._id,
 			},
